2d2fb0d0ce396c65fb8206616de8f8354b9a56a8
mattmcgiv <matt@antym.com>
2014-09-21 08:23:38 -0400

    Adds comment add/delete functionality.  Beginnings of viewing what's new.

--- mvmem-whats-new.php
+++ mvmem-whats-new.php
@@ -35,13 +35,17 @@ $mvmem_whats_new_db_version = '0.1';
 //Register actions
 register_activation_hook( __FILE__, 'mvmem_whats_new_db_install' );
 add_action('publish_page','mvmem_whats_new_publish_post');
-add_action('wp_head','mvmem_whats_new_mark_read');
+add_action('wp_head','mvmem_whats_new_mark_post_read');
+add_action('wp_head','mvmem_whats_new_mark_comment_read');
+add_action('wp_insert_comment', 'mvmem_whats_new_post_comment',99,2);
+
 
 function mvmem_whats_new_db_install() {//TODO: add similar comments table
 	global $wpdb;
 	global $mvmem_whats_new_db_version;
 
 	$table_name = $wpdb->prefix . 'mvmem_whats_new_posts';
+	$table_name_comments = $wpdb->prefix . 'mvmem_whats_new_comments';
 	
 	/*
 	 * Set charset and collate
@@ -67,7 +71,20 @@ function mvmem_whats_new_db_install() {//TODO: add similar comments table
 		UNIQUE KEY id (id)
 		) $charset_collate;";
 	}
-
+	
+	
+	
+	if( $wpdb->get_var("SHOW TABLES LIKE '" . $table_name_comments . "'") === $table_name_comments ) {
+	 // The database table exists, do nothing
+	} else {
+	 // Table does not exist, so create it
+	 	$sql = "CREATE TABLE $table_name_comments (
+		id bigint(20) NOT NULL AUTO_INCREMENT,
+		userid bigint(20) NOT NULL,		
+		commentid bigint(20) NOT NULL,
+		UNIQUE KEY id (id)
+		) $charset_collate;";
+	}
 
 	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 	dbDelta($sql);
@@ -100,6 +117,29 @@ function mvmem_whats_new_publish_post() {
 	
 }
 
+//When a new comment is posted, add it to each user's list
+function mvmem_whats_new_post_comment($comment_id, $comment_object) {
+	//if there are users
+	//Get all users
+	$mvmem_all_users = mvmem_whats_new_get_users();
+	
+	//Get commentID
+	//TODO: if it is just an update, as opposed to a new post, ignore it
+	//get the post object
+	$mvmem_current_comment_ID = $comment_id;
+	
+	foreach ($mvmem_all_users as $user) {
+		//get the user ID from the array and save it to a variable
+		$array = (array) $user;
+		$mvmem_user_ID = $array['ID'];
+		
+		//call func to write to db
+		mvmem_enqueue_whats_new_comment($mvmem_user_ID, $mvmem_current_comment_ID);
+		
+	}
+	
+}
+
 function mvmem_whats_new_get_users() {
  	$mvmem_users = get_users(array('fields'=>array('ID')));
 	
@@ -124,7 +164,25 @@ function mvmem_enqueue_whats_new ($userID, $postID) {
 	);
 }
 
-function mvmem_whats_new_mark_read() {
+function mvmem_enqueue_whats_new_comment ($userID, $commentID) {
+	global $wpdb;
+	
+	$table_name = $wpdb->prefix . 'mvmem_whats_new_comments';
+	
+	//TODO:if the table exists
+	//For each user
+		//add the userid and commentid as a row to the $table_name = $wpdb->prefix . 'mvmem_whats_new_comments';
+	
+	$wpdb->insert( 
+		$table_name, 
+		array( 
+			'userid' => $userID, 
+			'commentid' => $commentID,
+		) 
+	);
+}
+
+function mvmem_whats_new_mark_post_read() {
 	global $wpdb;
 	$mvmem_whats_new_current_user = get_current_user_id();
 	$mvmem_whats_new_postid = url_to_postid( get_permalink() );
@@ -146,5 +204,48 @@ function mvmem_whats_new_mark_read() {
 	//var_dump($mvmem_whats_new_current_user . " viewed " . $mvmem_whats_new_postid); die();
 	
 	
+}
+
+function mvmem_whats_new_mark_comment_read() {
+	global $wpdb;
+	$mvmem_whats_new_current_user = get_current_user_id();
+	
+	$mvmem_args = array(
+		'post_id' => url_to_postid(get_permalink()),
+	);
+	
+	$mvmem_whats_new_comments_this_page=get_comments($mvmem_args);
+	
+	$table_name = $wpdb->prefix . 'mvmem_whats_new_comments';
+	
+	foreach ($mvmem_whats_new_comments_this_page as $comment) {
+		$array = (array) $comment;
+		$mvmem_comment_ID = $array['comment_ID'];
+
+		//TODO:check if table exists before trying to delete from it
+		//remove entry for this user and page
+		$wpdb->query( 
+		$wpdb->prepare( 
+			"
+	         DELETE FROM $table_name
+			 WHERE userid = %d
+			 AND commentid = %s
+			",
+		        $mvmem_whats_new_current_user, $mvmem_comment_ID 
+	        )
+		);
+
+	}
+	
+		//var_dump($mvmem_whats_new_current_user . " viewed " . $mvmem_whats_new_postid); die();
+	
+	
+}
+
+function mvmem_whats_new_comment_approved($comment_id, $comment_object) {
+	
+	$var = $comment_id;
+	
+	var_dump($var); die();
 }
 
